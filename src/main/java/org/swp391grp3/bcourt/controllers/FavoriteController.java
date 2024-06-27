897bf46980862982e769e8c893ddc9ad2b2a1ceb
P4nzerr <enderthelord@gmail.com>
2024-06-27 19:10:36 +0700

    Add Favorite + Review

--- src/main/java/org/swp391grp3/bcourt/controllers/FavoriteController.java
+++ src/main/java/org/swp391grp3/bcourt/controllers/FavoriteController.java
@@ -1,10 +1,14 @@
 package org.swp391grp3.bcourt.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.swp391grp3.bcourt.dto.FavoriteDTO;
+import org.swp391grp3.bcourt.dto.ReviewDTO;
 import org.swp391grp3.bcourt.entities.Favorite;
+import org.swp391grp3.bcourt.entities.Review;
 import org.swp391grp3.bcourt.services.FavoriteService;
 
 import java.net.URI;
@@ -20,6 +24,14 @@ public class FavoriteController {
         Favorite createdFavorite = favoriteService.createFavorite(favorite);
         URI location = URI.create("/favorites/" + createdFavorite.getSaveId());
         return ResponseEntity.created(location).body(createdFavorite);
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<Page<FavoriteDTO>> getAllFavoritesByUserId(@RequestParam(value = "page", defaultValue = "0") int page,
+                                                                   @RequestParam(value = "size", defaultValue = "10") int size,
+                                                                   @PathVariable String userId) {
+        Page<Favorite> favorites = favoriteService.getAllFavoritesByUserId(page, size, userId);
+        return ResponseEntity.ok().body(favoriteService.favoriteDTOConverter(page, size, favorites));
     }
 
     @DeleteMapping("/delete/{saveId}")
